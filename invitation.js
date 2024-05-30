0a4a062bb34f3b1baf4c485b99fb66f8635051b7
imahrahim <imah.rahim@me.com>
2024-05-30 23:52:15 +0200

    mobile test

--- invitation.js
+++ invitation.js
@@ -1,8 +1,18 @@
 let playerName = localStorage.getItem('playerName');
 
 function sendResponse(response) {
-  let message = `Name: ${playerName}\nAntwort: ${response}`;
-  console.log(message);
-  alert('Deine Antwort wurde gesendet!');
-  window.location.href = 'gameover.html';
+  let templateParams = {
+    player_name: playerName,
+    message: response
+  };
+
+  emailjs.send('service_8l98fc7', 'template_5dmqkeo', templateParams)
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+       alert('Deine Antwort wurde gesendet!');
+       window.location.href = 'gameover.html';
+    }, function(error) {
+       console.log('FAILED...', error);
+       alert('Fehler beim Senden deiner Antwort.');
+    });
 }
